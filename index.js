bd0efac7923a80dd440683c9ebc1ea2e7aa25fc1
Yashaswini Shivathaya <evilgeniusprevails@gmail.com>
2023-11-20 10:45:08 +0530

    edit

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('server running at http://localhost:3000');
+server.listen(3001, () => {
+    console.log('server running at http://localhost:3001');
 });
